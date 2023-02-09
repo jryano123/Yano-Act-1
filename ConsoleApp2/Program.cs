74f869fa2d6282940577b8a15d547befcd40b6fc
jryano123 <johnreyano05@gmail.com>
2023-02-09 18:38:38 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            for( i = 1; i <= 300; i++)
+            {
+                if((i % 5) ==0 )
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
